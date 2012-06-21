1e48e5d18d130ae7d9f687d741d755a9885be280
Nico Güttler <nico.guettler@finalfrontierlabs.com>
2012-06-21 13:49:27 +0200

    added function to construct bwt.

--- project_3/exercise3.cpp
+++ project_3/exercise3.cpp
@@ -12,7 +12,6 @@
 #include <bitset>
 #include <math.h>
 #include <seqan/index.h> // include path muss be 'relative'
-
 using namespace std;
 /* Function prototypes (instead of header file) */
 //string readGenome(string &path);
@@ -20,26 +19,28 @@ using namespace std;
 /* Function definitions */
 string readGenome(string &path, string &seq_name) {
 
-    ifstream file;
-    string line, input;
+	ifstream file;
+	string line, input;
 
-    try {
-        file.open(path.c_str(), ifstream::in);
-        if (!file)
-                cout<< "File could not be opened. Please check path and file's name..."<< endl;
-        while (!file.eof()) {
-                getline(file, line);
-                if (line.at(0) != '>') // if not id line
-                        input.append(line);
-                else
-                    seq_name=line;
-            }
-    } catch (exception e) {
-            cout << e.what() << endl;
-    }
+	try {
+		file.open(path.c_str(), ifstream::in);
+		if (!file)
+			cout
+					<< "File could not be opened. Please check path and file's name..."
+					<< endl;
+		while (!file.eof()) {
+			getline(file, line);
+			if (line.at(0) != '>') // if not id line
+				input.append(line);
+			else
+				seq_name = line;
+		}
+	} catch (exception e) {
+		cout << e.what() << endl;
+	}
 
-    file.close();
-    return input;
+	file.close();
+	return input;
 }
 
 /* Running time calculation (C style)*/
@@ -60,6 +61,13 @@ void time_int(int print) {
 
 	t1 = t2;
 }
+
+void buildBWT(string &suffixArray, string &sequence, string &bwt) {
+
+	int sequenceLength = sequence.length();
+	cout << "Sequence length: " << sequenceLength;
+}
+
 /* Auxiliary functions to show content @TODO: remove if not used*/
 void printVector(vector<int> &v) {
 
@@ -131,75 +139,77 @@ void writeOutput(vector<vector<int> > &pos_score2) {
 /* ########################## MAIN ########################## */
 int main(int argc, char**argv) {
 
-    time_int(0); // start timing
-    string input_file, output_file, sequence, seq_name;
-    char mode;
-    //./exercise3 <input file> <output file> <mode>
-    /* Print the arguments */
-    if (argc > 1) {
-            cout << "Welcome..." << endl << "---- Introduced arguments ----"
-                            << endl;
-            for (int i = 1; i < argc; i++) {
-                    switch (i) {
-                    case 1:
-                            cout << "Input file:" << "\t" << argv[1] << endl;
-                            break;
-                    case 2:
-                            cout << "Output file:" << "\t" << argv[2] << endl;
-                            break;
-                    case 3:
-                            cout << "Mode:" << "\t\t" << argv[3] << endl;
-                            break;
-                    default:
-                            cout << "undefined input" << "\t" << argv[i] << endl;
-                            break;
-                    }
-            }
+	time_int(0); // start timing
+	string input_file, output_file, sequence, seq_name, bwt;
+	char mode;
+	//./exercise3 <input file> <output file> <mode>
+	/* Print the arguments */
+	if (argc > 1) {
+		cout << "Welcome..." << endl << "---- Introduced arguments ----"
+				<< endl;
+		for (int i = 1; i < argc; i++) {
+			switch (i) {
+			case 1:
+				cout << "Input file:" << "\t" << argv[1] << endl;
+				break;
+			case 2:
+				cout << "Output file:" << "\t" << argv[2] << endl;
+				break;
+			case 3:
+				cout << "Mode:" << "\t\t" << argv[3] << endl;
+				break;
+			default:
+				cout << "undefined input" << "\t" << argv[i] << endl;
+				break;
+			}
+		}
 
-            if (argc < 4)
-                    cout << "WARNING: not enough arguments given!" << endl;
-            /* set given arguments */
-            input_file = argv[1];
-            output_file = argv[2];
-            mode = argv[3][0]; // specify mode: c (compress) or x (extract). low case sensitive!
-            //mode = 'C';
-            if(mode!='c' && mode!='x')
-                cout << "WARNING: given mode is not defined!" << endl;
+		if (argc < 4)
+			cout << "WARNING: not enough arguments given!" << endl;
+		/* set given arguments */
+		input_file = argv[1];
+		output_file = argv[2];
+		mode = argv[3][0]; // specify mode: c (compress) or x (extract). low case sensitive!
+		//mode = 'C';
+		if (mode != 'c' && mode != 'x')
+			cout << "WARNING: given mode is not defined!" << endl;
 
-    }
-    /* mode c */
-    if(mode=='c'){
-        /* Read fasta files*/
-        cout << "---- Reading ----" << endl;
-        sequence = readGenome(input_file, seq_name);
+	}
+	/* mode c */
+	if (mode == 'c') {
+		/* Read fasta files*/
+		cout << "---- Reading ----" << endl;
+		sequence = readGenome(input_file, seq_name);
+		sequence.append("$");
 
-    //    [x] reads a single sequence from a fasta file
-    //    [] calculates the BWT of that sequence
-    //    [] implements move-to-front encoding and Huffman coding to compress the BWT
-    //    [] writes the Huffman code into an outfile 
+		//    [x] reads a single sequence from a fasta file
+		//    [] calculates the BWT of that sequence
+		//    [] implements move-to-front encoding and Huffman coding to compress the BWT
+		//    [] writes the Huffman code into an outfile
 //        string testo = "hello world!";
 //        ::seqan::String<char> text = testo;
-        ::seqan::String<char> text = sequence;
-	::seqan::String<char> pattern = "l";
-	::seqan::String<unsigned> sa;
+		::seqan::String<char> text = sequence;
+		::seqan::String<char> pattern = "l";
+		::seqan::String<unsigned> suffixArray;
 
+		::seqan::resize(suffixArray, ::seqan::length(text));
+		::seqan::createSuffixArray(suffixArray, text, ::seqan::Skew7());
+		cout << suffixArray[2] << endl;
 
-	::seqan::resize(sa, ::seqan::length(text));
-	::seqan::createSuffixArray(sa, text, ::seqan::Skew7());
-        cout << sa[2]<<endl;
-    }else if(mode=='x'){
-        /* mode x */
-    
+		buildBWT(suffixArray, sequence, bwt);
+
+	} else if (mode == 'x') {
+		/* mode x */
+
 //    [] reads a file containing the BWT compression of some sequence
 //    [] writes the uncompressed sequence into a fasta outfile. 
+	}
+	/* Ending */
+	cout << "---- Results ---- " << endl;
+	cout << "Name of the sequence: " << seq_name << endl;
+	cout << "Sequence: " << sequence.substr(0, 10) << endl;
 
-    }
-    /* Ending */
-    cout << "---- Results ---- " << endl;
-    cout<<"Name of the sequence: " <<seq_name<<endl;
-    cout<<"Sequence: " <<sequence.substr(0,10)<<endl;
-    
-    time_int(1); // print out elapsed time
-    cout << endl;
-    return 0;
+	time_int(1); // print out elapsed time
+	cout << endl;
+	return 0;
 }
